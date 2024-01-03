07669909e404a5ac43e827106e98c85269dc76bc
Matěj <mzel@ciklum.com>
2024-01-03 18:57:10 +0100

    Tailwind testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,24 @@ module.exports = {
   content: ['./dist/*.html'],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '0rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'green': '#004721',
+      'yellow': '#CCAC00',
+      'silver': '#FFFBE5',
+    },
   },
   variants: {
     extend: {},
