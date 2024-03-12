519f33d459e3394e7b846f582d2f269fbbd43620
Matěj Zelenka <matej.zel@seznam.cz>
2024-03-12 20:33:14 +0100

    Added anchor nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        'jomolhari': ['"Jomolhari"', 'sans-serif']
+      },
       boxShadow: {
         '3xl': '0px 4px 16px rgba(17,17,26,0.1), 0px 8px 24px rgba(17,17,26,0.1), 0px 16px 56px rgba(17,17,26,0.1)' 
        },
