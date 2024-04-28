f762008605cfdc1307a73d88c750fec86e2e997a
Matěj <mzel@ciklum.com>
2024-04-28 17:54:06 +0200

    Small changes, need to fix form

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
 import "/style.css";
 import "add-to-calendar-button";
-import {addLoadingAnimation, resendForm, showAlert} from "/js/formUtils.js?url";
-import {countdownInit} from "/js/countdown.js?url";
 
-countdownInit();
+
+const addLoadingAnimation = (form) => {
+  const spinner = form.querySelector('svg');
+  const inputs = [...form.querySelectorAll('input')].concat([form.querySelector('textarea')]);
+  spinner.classList.remove('hidden');
+  spinner.classList.add('inline');
+  inputs.length >= 0 && inputs.forEach(item => item.disabled = true);
+}
+
+const showAlert = (isSuccesfullRequest, form, alerts) => {
+  form.classList.add('hidden');
+  alerts.classList.remove('hidden');
+  alerts.classList.add('grid');
+  if (isSuccesfullRequest) {
+    alerts.querySelector('#alert-success').classList.remove('hidden');
+  } else {
+    alerts.querySelector('#alert-failure').classList.remove('hidden');
+  }
+}
+
+
+const resendForm = (form, alerts) => {
+  const spinner = form.querySelector('svg');
+
+  if (form.classList.contains('hidden')) {
+    form.classList.remove('hidden');
+    form.classList.add('grid');
+    alerts.classList.remove('grid');
+    alerts.classList.add('hidden');
+    spinner.classList.remove('inline');
+    spinner.classList.add('hidden');
+  }
+}
+
 
 //url used from Google sheet app
 const scriptURL =
@@ -16,7 +47,7 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   addLoadingAnimation(form);
 
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+  fetch(scriptURL, {method: "POST", body: new FormData(form)})
     .then((response) => {
       console.log("Success!", response);
       showAlert(true, form, alerts);
@@ -28,3 +59,50 @@ form.addEventListener("submit", (e) => {
 });
 
 resendFormButton.addEventListener("click", () => resendForm(form, alerts));
+
+
+const countdownInit = () => {
+  const countdownDiv = document.querySelector("#countdown");
+  const countdownDate = new Date("Sep 25, 2024 17:00:00").getTime();
+  const timeInterval = setInterval(function () {
+    let dateNow = new Date().getTime();
+    let timeRemaining = countdownDate - dateNow;
+    countdownDiv.innerHTML = `${getDays(timeRemaining)}  ${getHours(
+      timeRemaining
+    )}  ${getMinutes(timeRemaining)}  ${getSeconds(timeRemaining)}`;
+    if (timeRemaining > 0) {
+      clearInterval(timeInterval)
+    }
+  }, 1000);
+};
+
+const getDays = (distance) => {
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  return days + getUnitInflection(days, "d", "d", "d");
+};
+const getHours = (distance) => {
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  return hours + getUnitInflection(hours, "h", "h", "h");
+};
+const getMinutes = (distance) => {
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  return minutes + getUnitInflection(minutes, "min", "min", "min");
+};
+const getSeconds = (distance) => {
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  return seconds + getUnitInflection(seconds, "s", "s", "s");
+};
+
+const getUnitInflection = (count, five, two, one) => {
+  let unit = five;
+  if (count > 1 && count < 5) {
+    unit = two;
+  } else if (count === 1) {
+    unit = one;
+  }
+  return "&nbsp;" + unit;
+};
+
+countdownInit();
